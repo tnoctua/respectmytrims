ee0a187950a3c6fdca101a96ebe707013a5ad204
N <24514810+tnoctua@users.noreply.github.com>
2025-05-26 15:15:14 -0400

    1.20 Backport Supports versions 1.20 and 1.20.1

--- src/main/java/me/tnoctua/respectmytrims/mixin/PiglinBrainMixin.java
+++ src/main/java/me/tnoctua/respectmytrims/mixin/PiglinBrainMixin.java
@@ -1,32 +1,38 @@
 package me.tnoctua.respectmytrims.mixin;
 
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.AttributeModifierSlot;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PiglinBrain;
-import net.minecraft.item.equipment.trim.ArmorTrim;
-import net.minecraft.item.equipment.trim.ArmorTrimMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.trim.ArmorTrim;
+import net.minecraft.item.trim.ArmorTrimMaterial;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
+
 @Mixin(PiglinBrain.class)
 public abstract class PiglinBrainMixin {
 
-    @Inject(method = "isWearingPiglinSafeArmor", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "wearsGoldArmor", at = @At("RETURN"), cancellable = true)
     private static void isWearingPiglinSafeArmor(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
         // Gold trims pacify piglins
-        for (EquipmentSlot equipmentSlot : AttributeModifierSlot.ARMOR) {
-            ArmorTrim components = entity.getEquippedStack(equipmentSlot).getComponents().get(DataComponentTypes.TRIM);
-            if (components != null) {
-                RegistryEntry<ArmorTrimMaterial> trim = components.material();
-                if (trim != null && trim.getIdAsString().equals("minecraft:gold")) {
-                    cir.setReturnValue(true);
-                }
-            }
+        DynamicRegistryManager registries = entity.getWorld().getRegistryManager();
+        for (ItemStack stack : entity.getArmorItems()) {
+            Optional<ArmorTrim> optionalTrim = ArmorTrim.getTrim(registries, stack);
+            optionalTrim.ifPresent(trim -> {
+                RegistryEntry<ArmorTrimMaterial> materialEntry = trim.getMaterial();
+                materialEntry.getKey().ifPresent(key -> {
+                    ArmorTrimMaterial material = registries.get(RegistryKeys.TRIM_MATERIAL).get(key);
+                    if (material != null && material.assetName().equals("gold")) {
+                        cir.setReturnValue(true);
+                    }
+                });
+            });
         }
     }
 
