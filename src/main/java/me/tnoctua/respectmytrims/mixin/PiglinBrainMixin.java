37778230854e79b13ebd760c53d0359f752bf7cf
N <24514810+tnoctua@users.noreply.github.com>
2025-05-26 13:28:31 -0400

    1.21.2 Backport Supports versions 1.21.2, 1.21.3, and 1.21.4

--- src/main/java/me/tnoctua/respectmytrims/mixin/PiglinBrainMixin.java
+++ src/main/java/me/tnoctua/respectmytrims/mixin/PiglinBrainMixin.java
@@ -1,10 +1,9 @@
 package me.tnoctua.respectmytrims.mixin;
 
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.AttributeModifierSlot;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PiglinBrain;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.equipment.trim.ArmorTrim;
 import net.minecraft.item.equipment.trim.ArmorTrimMaterial;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -19,8 +18,8 @@ public abstract class PiglinBrainMixin {
     @Inject(method = "isWearingPiglinSafeArmor", at = @At("RETURN"), cancellable = true)
     private static void isWearingPiglinSafeArmor(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
         // Gold trims pacify piglins
-        for (EquipmentSlot equipmentSlot : AttributeModifierSlot.ARMOR) {
-            ArmorTrim components = entity.getEquippedStack(equipmentSlot).getComponents().get(DataComponentTypes.TRIM);
+        for (ItemStack stack : entity.getArmorItems()) {
+            ArmorTrim components = stack.getComponents().get(DataComponentTypes.TRIM);
             if (components != null) {
                 RegistryEntry<ArmorTrimMaterial> trim = components.material();
                 if (trim != null && trim.getIdAsString().equals("minecraft:gold")) {
